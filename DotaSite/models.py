5de9435014c0e6d63c41cf551434e36b4a1001a7
ShahkarHassan <62255873+ShahkarHassan@users.noreply.github.com>
2020-12-20 11:44:38 +0500

    added

--- DotaSite/models.py
+++ DotaSite/models.py
@@ -8,122 +8,13 @@
 from django.db import models
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
 
 class DotaAdmin(models.Model):
     admin = models.ForeignKey('DotaUser', models.DO_NOTHING, primary_key=True)
     admin_registration_number = models.CharField(unique=True, max_length=20)
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_admin'
 
 
@@ -132,7 +23,7 @@ class DotaGamer(models.Model):
     gamer_ign = models.CharField(max_length=20)
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_gamer'
 
 
@@ -142,7 +33,7 @@ class DotaGamerMatchTournament(models.Model):
     tournamentid = models.ForeignKey('DotaTournament', models.DO_NOTHING, db_column='tournamentid', blank=True, null=True)
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_gamer_match_tournament'
 
 
@@ -156,7 +47,7 @@ class DotaMatch(models.Model):
     match_assist = models.IntegerField()
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_match'
 
 
@@ -166,7 +57,7 @@ class DotaMmr(models.Model):
     mmr_medal = models.CharField(max_length=30)
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_mmr'
 
 
@@ -176,7 +67,7 @@ class DotaPremiumuser(models.Model):
     premiumuser_registrationexpirydate = models.CharField(db_column='premiumuser_RegistrationExpiryDate', max_length=30)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_premiumuser'
 
 
@@ -187,7 +78,7 @@ class DotaTournament(models.Model):
     tournament_prize = models.IntegerField(db_column='Tournament_prize', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        
         db_table = 'dota_tournament'
 
 
@@ -199,5 +90,4 @@ class DotaUser(models.Model):
     user_password = models.CharField(max_length=30)
 
     class Meta:
-        managed = False
         db_table = 'dota_user'
